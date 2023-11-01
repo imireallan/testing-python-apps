8b90e8cad09cd20913821e21ad642c2dc6fc0dd8
Allan Imire <allanimire@gmail.com>
2023-11-01 19:00:38 +0300

    add app,blog and post modules

---
+++ blog.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+from typing import List
+from post import Post
+
+# class Blog:
+#     def __init__(self, title, author, posts = None) -> None:
+#         self.title = title
+#         self.author = author
+#         if posts is None:
+#             self.posts = []
+#         else:
+#             self.posts = posts
+
+@dataclass
+class Blog:
+    title: str
+    author: str
+    posts: List[Post] = field(default_factory=list)
+
+    def create_post(self, post: Post):
+        self.posts.append(post)
+
+    def __str__(self) -> str:
+        return f'{self.title} by {self.author} ({len(self.posts)} {"posts" if len(self.posts) == 0 or len(self.posts) > 1 else "post"})'
---
+++ post.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+# class Post:
+#     def __init__(self, title, content) -> None:
+#         self.title = title
+#         self.content = content
+
+#     def to_dict(self):
+#         return {
+#             'title': self.title,
+#             'content': self.content,
+#         }
+    
+
+@dataclass
+class Post:
+    title: str
+    content: str
+
+    def to_dict(self):
+        return {
+            'title': self.title,
+            'content': self.content,
+        }
